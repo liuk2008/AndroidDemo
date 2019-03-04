3abfdd26c1fa3eb5ca6209d862c9711e86f4dbff
liuk <471636288@qq.com>
2019-03-04 18:53:22 +0800

    增加WebView使用demo

--- app/src/main/java/com/android/demo/WebViewActivity.java
+++ app/src/main/java/com/android/demo/WebViewActivity.java
@@ -1,16 +1,21 @@
 package com.android.demo;
 
-import android.net.Uri;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebView;
 
-import com.android.common.base.activity.CoreActivity;
 import com.android.common.h5.WebViewHelper;
+import com.android.common.h5.client.MyWebChromeClient;
 import com.android.common.h5.client.MyWebViewClient;
 import com.android.common.h5.client.WebViewUtils;
 import com.android.utils.common.LogUtils;
 
-public class WebViewActivity extends CoreActivity {
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+public class WebViewActivity extends AppCompatActivity {
 
     private static final String TAG = WebViewActivity.class.getSimpleName();
     private WebViewHelper webViewHelper;
@@ -18,10 +23,10 @@ public class WebViewActivity extends CoreActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        configStatusBar(R.drawable.shape_titlebar);
         webViewHelper = WebViewHelper.create(this);
         setContentView(webViewHelper.getRootView());
 
+        // 设置cookie
         WebViewUtils.setCookie("token", "1F99DF52AFFD474CB5C5331C86AFB4CA.10D5D0054A8C144C38693F714694BF2C");
         WebViewUtils.setCookie("currentUserId", "ED08E7A6C6F7459FA264C736482CD5F6");
         WebViewUtils.setCookie("origin", "android");
@@ -34,34 +39,44 @@ public class WebViewActivity extends CoreActivity {
             @Override
             public boolean onLoadUrl(WebView webView, String url) {
                 LogUtils.logd(TAG, "onLoadUrl:" + url);
-                Uri uri = Uri.parse(url);
-                String host = uri.getHost();
-                if ("returnToSpecifiedPage".equals(host)) {
-                    finish();
-                    return true;
-                } else {
-                    return false;
-                }
+                return false;
             }
 
             @Override
             public void executorJs(WebView webView, String url) {
+                LogUtils.logd(TAG, "executorJs: ");
             }
         });
 
-        /*
-         * 为WebView添加可供JS调用的本地方法。WebView.addJavascriptInterface(Object object, String name)
-         * object参数的实现是一个普通的JavaClass,name参数会被绑定成为js中window对象的一个属性，该属性指向object参数,
-         * 因此js中可以通过window.name.methodName()调用object对象有的本地方法。
-         * window.CallAppFunction.onReceiveTitleInfo
-         */
-        webViewHelper.addJavascriptInterface(null,"");
+        webViewHelper.setWebChromeClientInterface(new MyWebChromeClient.OpenFileChooserCallBack() {
+            // ===============点击表单选择图片或者拍照==============
+            @Override
+            public void showPicWindow() {
+                webViewHelper.showPicWindow();
+            }
+        });
 
- //       webView.loadUrl("https://appapp.snxw.com/sy/tj_3696/201809/t20180930_376601.html");
-//        webView.loadUrl("http://jfx.qdfaw.com:6180/truck-qingdao-web/");
-        webViewHelper.load("https://h5.lawcert.com/trade/withdraw");
+        String url = "http://jfx.qdfaw.com:6180/truck-qingdao-web/";
+        String token = "00bdd0c39498430e94801beb6578ef6dg";
+        String appType = "2", userType = "1", platform = "2";
+        try {
+            token = URLEncoder.encode(token, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        url = String.format("%s?token=%s&appType=%s&userType=%s&platform=%s&cityNmae=%s", url,
+                token, appType, userType, platform, "西安");
+
+        webViewHelper.load(url);
+//       webView.loadUrl("https://appapp.snxw.com/sy/tj_3696/201809/t20180930_376601.html");
+//       webViewHelper.load("https://h5.lawcert.com/trade/withdraw");
 
     }
 
-
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        webViewHelper.setPicData(requestCode, resultCode, data);
+    }
+    
 }
