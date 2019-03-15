67df3cc2d406868baf9f95981868793fabcadad3
liuk <471636288@qq.com>
2019-03-15 14:45:06 +0800

    增加RefreshView使用demo

--- app/src/main/java/com/android/demo/fragment/HomeFragment.java
+++ app/src/main/java/com/android/demo/fragment/HomeFragment.java
@@ -1,27 +1,35 @@
 package com.android.demo.fragment;
 
-import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.RequiresApi;
+import android.os.SystemClock;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.android.common.base.fragment.BaseFragment;
-import com.android.common.view.StatusBarUtils;
-import com.android.common.view.ToolbarUtil;
+import com.android.common.refreshview.MyCommonAdapter;
+import com.android.common.refreshview.MyRefreshView;
+import com.android.common.refreshview.MyViewHolder;
+import com.android.common.utils.StatusBarUtils;
+import com.android.common.utils.ToolbarUtil;
 import com.android.demo.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * 状态栏透明
+ * 1、设置标题栏根据页面滑动渐变
+ * 2、测试下拉刷新、上拉加载效果
  */
 public class HomeFragment extends BaseFragment {
 
     private static final String TAG = HomeFragment.class.getSimpleName();
     private View viewTitle;
     private int scrollMaxHeight;
+    private MyRefreshView refreshView;
+    private MyCommonAdapter<String> adapter;
+    private int index = 1;
 
     {
         super.TAG = TAG;
@@ -33,26 +41,19 @@ public class HomeFragment extends BaseFragment {
     }
 
     @Override
-    @RequiresApi(Build.VERSION_CODES.M)
     public void initRootViews(View baseRootView) {
         super.initRootViews(baseRootView);
         ToolbarUtil.configTitlebar(baseRootView, "首页", View.GONE);
-        ViewGroup view = baseRootView.findViewById(R.id.view_test);
-        for (int i = 0; i < 60; i++) {
-            TextView textView = new TextView(getContext());
-            textView.setText("测试" + i);
-            textView.setTextColor(Color.BLACK);
-            view.addView(textView, i);
-        }
-        View scrollView = baseRootView.findViewById(R.id.scrollView);
+        refreshView = baseRootView.findViewById(R.id.refreshView);
         viewTitle = baseRootView.findViewById(R.id.view_title);
+
         viewTitle.setAlpha(0);
         // 设置系统状态栏透明，通过设置标题栏Padding高度=状态栏高度，保持颜色一致
         viewTitle.setPadding(0, StatusBarUtils.getStatusBarHeight(getActivity()), 0, 0);
         scrollMaxHeight = (getResources().getDisplayMetrics().heightPixels) / 3;
-        scrollView.setOnScrollChangeListener(new View.OnScrollChangeListener() {
+        refreshView.setOnScrollChangeListener(new MyRefreshView.OnScrollChangeListener() {
             @Override
-            public void onScrollChange(View v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
+            public void onScrollChange(RecyclerView recyclerView, int scrollX, int scrollY) {
                 float alpha = 0;
                 if (scrollY <= 0) {
                     viewTitle.setAlpha(0);
@@ -64,5 +65,66 @@ public class HomeFragment extends BaseFragment {
                 }
             }
         });
+
+        adapter = new MyCommonAdapter<String>(getContext(), R.layout.item_view) {
+            @Override
+            public void convert(MyViewHolder holder, String data, int position) {
+                holder.setText(R.id.item_title, data);
+            }
+        };
+
+        refreshView.refreshStart();
+        refreshView.setAdapter(adapter);
+        setData("测试");
+
+        refreshView.setOnRefreshListener(new MyRefreshView.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                index = 1;
+                setData("下拉刷新");
+            }
+        });
+
+        refreshView.setOnLoadMoreListener(new MyRefreshView.OnLoadMoreListener() {
+            @Override
+            public void onLoadMore() {
+                index++;
+                if (index <= 2)
+                    setData("上拉加载");
+
+            }
+        });
+
+        adapter.setOnItemClickListener(new MyCommonAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+            }
+        });
+
     }
+
+    private void setData(final String name) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                SystemClock.sleep(3000);
+                final List<String> list = new ArrayList<>();
+                for (int i = 0; i < 60; i++) {
+                    list.add(name + i);
+                }
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if ("下拉刷新".equals(name)) {
+                            adapter.cleanData();
+                        }
+                        adapter.appendData(list);
+                        refreshView.setLoadMore(index < 2);
+                        refreshView.refreshComplete();
+                    }
+                });
+            }
+        }).start();
+    }
+
 }
